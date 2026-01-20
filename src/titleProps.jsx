25baefc69269604dad23b11ffec4f6f8e4e2d7c1
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-20 23:47:07 +0430

    Fix list rendering in ProductTitle component and correct feature2 label

--- src/titleProps.jsx
+++ src/titleProps.jsx
@@ -1,12 +1,14 @@
 function ProductTitle({title, name, features1, feature2}) {
-    const list = features1.map((feature)=> <li style={}>{feature}</li>)
+    const list = features1.map((feature)=> <li style={{listStyle:"none"}} key={feature}>{feature}</li>)
+
+
   return (
     <>
     <div style={{display: "flex", gap: '1rem', alignItems: "center", border:"1px solid white", margin:"1rem", padding:"0.3rem 0.9rem"}}>
         <p>{title}</p>
     <h5>I am: {name}</h5>
     <h6>Feature: {list}</h6>
-    <h6>Feature: {feature2}</h6>
+    <h6>Feature2: {feature2}</h6>
     </div>
     </>
   )
