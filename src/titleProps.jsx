cb171445a426e632268b79431e6fba31db757bf3
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-20 23:12:25 +0430

    Update ProductTitle component to display name alongside title

--- src/titleProps.jsx
+++ src/titleProps.jsx
@@ -1,5 +1,10 @@
 function ProductTitle(props) {
-  return <p>{props.title}</p>;
+  return (
+    <>
+    <p>{props.title}</p>;
+    <h5>My name is: {props.name}</h5>
+    </>
+  )
 }
 
 export default ProductTitle;
