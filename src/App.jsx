88a0ebf9845df8750a140d5e503321c5598ca658
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-20 23:52:44 +0430

    Fix price value for the third product in ProductTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ let option2 ={a: "feature A", b: "feature B", c: "feature C"}
 
       <ProductTitle title="this is the first product title" name="product one" features1={option1} feature2={option2.a} price={67000}/>
       <ProductTitle title="this is the second product title" name="product two" features1={option1} feature2={option2.b} price={50000}/>
-      <ProductTitle title="this is the third product title" name="product three" features1={option1} feature2={option2.c} price={45000}/>
+      <ProductTitle title="this is the third product title" name="product three" features1={option1} feature2={option2.c} price={450}/>
       <ProductTitle title="this is the fourth product title" name="product four" features1={option1} feature2={option2.c} price={35000}/>
     </>
   )
