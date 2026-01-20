38b4079d6ba86549655102cca23f03b4b074afb0
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-20 23:27:45 +0430

    Update padding in ProductTitle component for improved spacing

--- src/titleProps.jsx
+++ src/titleProps.jsx
@@ -1,7 +1,7 @@
 function ProductTitle({title, name, features1, feature2}) {
   return (
     <>
-    <div style={{display: "flex", gap: '1rem', alignItems: "center", border:"1px solid white", margin:"1rem", padding:"0.6rem"}}>
+    <div style={{display: "flex", gap: '1rem', alignItems: "center", border:"1px solid white", margin:"1rem", padding:"0.3rem 0.9rem"}}>
         <p>{title}</p>
     <h5>I am: {name}</h5>
     <h6>Feature: {features1}</h6>
