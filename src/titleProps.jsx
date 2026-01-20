4c308dc477a7dce96e2a1284be56bc54f3feaeff
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-20 23:49:44 +0430

    Add price prop to ProductTitle component and display discount status

--- src/titleProps.jsx
+++ src/titleProps.jsx
@@ -1,6 +1,7 @@
-function ProductTitle({title, name, features1, feature2}) {
+function ProductTitle({title, name, features1, feature2, price}) {
     const list = features1.map((feature)=> <li style={{listStyle:"none"}} key={feature}>{feature}</li>)
 
+    let isDiscounted = price>20000? "10% Discount Applied": "No Discount";
 
   return (
     <>
@@ -9,6 +10,8 @@ function ProductTitle({title, name, features1, feature2}) {
     <h5>I am: {name}</h5>
     <h6>Feature: {list}</h6>
     <h6>Feature2: {feature2}</h6>
+    <h6>Price: {price}</h6>
+    <h6>{isDiscounted}</h6>
     </div>
     </>
   )
